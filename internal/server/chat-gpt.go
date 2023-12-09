6d6d4b1529b54016d9bc25c8262fbd4811e44c90
GoBig87 <inbody.5@gmail.com>
2023-12-09 13:44:02 -0800

    check in

--- internal/server/chat-gpt.go
+++ internal/server/chat-gpt.go
@@ -10,18 +10,21 @@ import (
 	"log"
 )
 
-func MakeChatGptServer() *ChatGptServer {
-	return &ChatGptServer{}
+func MakeChatGptServer(apiKey, orgId, endpoint string) *ChatGptServer {
+	return &ChatGptServer{
+		client: gpt.NewChatGptClient(apiKey, orgId, endpoint),
+	}
 }
 
 type ChatGptServer struct {
+	client *gpt.ChatGptClient
 	api.UnimplementedChatGptServiceServer
-}
+}git 
 
 func (s *ChatGptServer) ProcessPrompt(ctx context.Context, req *api.ProcessPromptRequest) (*api.ProcessPromptResponse, error) {
 	log.Printf("Received prompt: %s\n", req.Prompt)
 	prompt := req.Prompt
-	resp, err := gpt.PromptChatGPT(prompt)
+	resp, err := s.client.PromptChatGPT(prompt)
 	if err != nil {
 		log.Fatalf("Error in ProcessPrompt: %v", err)
 		return nil, status.Error(codes.Internal, fmt.Sprintf("Error processing prompt: %v", err))
