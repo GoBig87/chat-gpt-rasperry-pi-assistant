eb13f5a6e25d0f5ce125484d73923eef5f25c850
GoBig87 <inbody.5@gmail.com>
2023-12-16 17:43:23 -0800

    building installer for systemd

--- installer.sh
+++ installer.sh
@@ -221,3 +221,23 @@ if [ ! -d "$wake_word_dir" ]; then
 fi
 
 cp wake-words/* "$wake_word_dir"
+
+# Get the current username
+USERNAME=$USER
+
+# Replace <user> with $USERNAME in the service files
+sed "s/<user>/$USERNAME/g" gpt-api.service > gpt-api.service.tmp
+sed "s/<user>/$USERNAME/g" gpt-app.service > gpt-app.service.tmp
+
+# Copy the modified service files to /etc/systemd/system/
+sudo cp ./packaging/gpt-api.service.tmp /etc/systemd/system/gpt-api.service
+sudo cp ./packaging/gpt-app.service.tmp /etc/systemd/system/gpt-app.service
+
+# Reload systemd to pick up the changes
+sudo systemctl daemon-reload
+
+# Enable and start the services
+sudo systemctl enable gpt-api.service
+sudo systemctl start gpt-api.service
+sudo systemctl enable gpt-app.service
+sudo systemctl start gpt-app.service
