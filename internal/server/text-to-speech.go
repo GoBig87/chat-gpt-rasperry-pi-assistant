2dc0bc391dc5c9aab803a297aea475333b110541
GoBig87 <inbody.5@gmail.com>
2023-12-16 16:17:16 -0800

    removing error code possibly

--- internal/server/text-to-speech.go
+++ internal/server/text-to-speech.go
@@ -20,21 +20,21 @@ func (t *TextToSpeechServer) ProcessText(req *api.ProcessTextRequest, stream api
 	log.Printf("process text request: %v\n", req)
 	// Use a channel to signal when transcription is complete
 	transcriptionComplete := make(chan bool)
-	var transcribeError error
-	var transcribedText string
+	defer close(transcriptionComplete)
+	transcriptionError := make(chan error)
+	defer close(transcriptionError)
 	// Send initial processing message
 	stream.Send(&api.ProcessTextResponse{Processed: false})
 	text := req.Text
 
 	// Run TranscribeText in a goroutine
 	go func() {
-		transcribeError = t2s.TranscribeText(text)
-		if transcribeError != nil {
-			fmt.Println("Error in TranscribeSpeech:", transcribeError)
-			transcriptionComplete <- true
+		err := t2s.TranscribeText(text)
+		if err != nil {
+			fmt.Println("Error in TranscribeSpeech:", err)
+			transcriptionError <- err
 			return
 		}
-		log.Println(transcribedText)
 		transcriptionComplete <- true
 	}()
 
@@ -45,6 +45,9 @@ func (t *TextToSpeechServer) ProcessText(req *api.ProcessTextRequest, stream api
 			stream.Send(&api.ProcessTextResponse{Processed: true})
 			log.Printf("Text to speech complete\n")
 			return nil
+		case err := <-transcriptionError:
+			log.Printf("Error transcribing text: %v\n", err)
+			return err
 		case <-time.After(1 * time.Second):
 			// Send processing update every second
 			stream.Send(&api.ProcessTextResponse{Processed: false})
